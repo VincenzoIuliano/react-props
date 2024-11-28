99be75f5d032ea09ca0f3db170b7bc9077615d9c
VincenzoIuliano <bubucane@outlook.it>
2024-11-28 17:40:19 +0100

    ADD: props

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,13 +2,20 @@ import Button from '../Button/Button.jsx'
 import style from '../Card/Card.module.css'
 import imagePost from '../../assets/paesaggio.jpeg'
 
-export default function Card () {
+export default function Card (props) {
+    // console.log(props);
+    
+    const card_tags = props.tags ? props.tags.join(', ') : ''
+    console.log(card_tags);
+    
+
     return (
         <div className={style.card}>
             <img className={style.image} src={imagePost} alt="" />
             <div className={style.card_body}>
-                <h3 className={style.card_title}>Titolo del post</h3>
-                <p className={style.card_description}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam commodi iure quibusdam nam quis laudantium odit odio eius reiciendis corporis!</p>
+                <h3 className={style.card_title}>{props.title}</h3>
+                <h4 className={style.card_tags}>Tags: {card_tags}</h4>
+                <p className={style.card_description}>{props.content}</p>
                 <Button />
             </div>
         </div>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,16 +1,22 @@
 import Card from '../Card/Card'
 import style from '../Main/Main.module.css'
+import {posts} from '../../posts.js'
 
 
 export default function Main() {
+    
+    // const tags = props.tags.join(', ')
+
     return (
         <main>
             <section className={style.section}>
                 <div className="container">
                     <div className="row">
-                        <div className="col-6">
-                            <Card />
-                        </div>
+                        {posts.map((post) => (
+                            <div key={post.id} className="col-6">
+                                <Card title={post.title} content={post.content} />
+                            </div>
+                        ))}
                     </div>
                 </div>
             </section>
